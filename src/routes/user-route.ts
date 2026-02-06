1c5a5d2e3b6f8f4bb67a95ce716721482dfdcd69
Ridwan <ridwanpr@outlook.com>
2026-02-07 01:46:19 +0700

    auth middleware

--- src/routes/user-route.ts
+++ src/routes/user-route.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { userController } from "../config/container.js";
+import { authenticate } from "../http/middlewares/auth-middleware.js";
 
 const userRouter = Router();
 
-userRouter.get("/:email", userController.getUser);
+userRouter.get("/:email", authenticate, userController.getUser);
 
 export { userRouter };
